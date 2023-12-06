380abe66dcd9d1c51f65f1fdb86c14f223751683
Toi Tran <toitv.astary@gmail.com>
2023-12-06 08:27:52 +0700

    gomax project

---
+++ assets/index.js
@@ -0,0 +1,15 @@
+function tabOptions(evt, option) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+      document.getElementById(option).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
